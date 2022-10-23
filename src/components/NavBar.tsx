f17bedd39f040824790a187f3f46b9cc78f806f3
lazybump <104283344+lazybump@users.noreply.github.com>
2022-10-23 19:22:39 +0100

    feat display cart total only when 1 or more

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -26,9 +26,11 @@ export function NavBar() {
               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
             />
           </svg>
-          <div className="bg-red-600 h-5 w-5 rounded-full absolute bottom-0 right-0 text-sm flex items-center justify-center text-white translate-x-1/4 translate-y-1/4">
-            {cartQuantity || 0}
-          </div>
+          {cartQuantity ? (
+            <div className="bg-red-600 h-5 w-5 rounded-full absolute bottom-0 right-0 text-sm flex items-center justify-center text-white translate-x-1/4 translate-y-1/4">
+              {cartQuantity}
+            </div>
+          ) : null}
         </button>
       </div>
     </nav>
