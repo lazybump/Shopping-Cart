15410bfdd05044d4b4295726b96287fd7b46db50
lazybump <104283344+lazybump@users.noreply.github.com>
2022-10-23 03:47:42 +0100

    implement add to cart/remove from cart functionality

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,17 +1,23 @@
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 interface StoreItemProps {
+  id: number;
   name: string;
   price: number;
   imgUrl: string;
 }
 
-export function StoreItem({ name, price, imgUrl }: StoreItemProps) {
-  const quantity = 2;
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const { getItemQuantity, incrementItem, decrementItem } = useShoppingCart();
+  const quantity = getItemQuantity(id);
+
+  // Continue from 40:54
+
   return (
     <div className="bg-white rounded overflow-hidden shadow md:max-w-md">
       <img src={imgUrl} className="h-52 w-full object-cover" />
-      <div className="px-4 pt-3">
+      <div className="px-4 py-3">
         <div className="h-14 flex justify-between">
           <span className="text-2xl font-semibold">{name}</span>
           <span className="text-lg text-slate-500 font-medium">
@@ -20,20 +26,29 @@ export function StoreItem({ name, price, imgUrl }: StoreItemProps) {
         </div>
         <div>
           {quantity === 0 ? (
-            <button className="bg-blue-500 text-white w-full rounded-sm h-10">
+            <button
+              className="bg-blue-500 text-white w-full rounded-sm h-10"
+              onClick={() => incrementItem(id)}
+            >
               + Add To Cart
             </button>
           ) : (
             <div className="flex flex-col items-center gap-3">
               <div className="flex gap-3">
-                <button className="bg-blue-500 text-white px-3 py-2 rounded">
+                <button
+                  className="bg-blue-500 text-white px-3 py-2 rounded"
+                  onClick={() => decrementItem(id)}
+                >
                   -
                 </button>
                 <div className="flex items-end gap-2">
                   <span className="text-2xl">{quantity}</span>
                   in cart
                 </div>
-                <button className="bg-blue-500 text-white px-3 py-2 rounded">
+                <button
+                  className="bg-blue-500 text-white px-3 py-2 rounded"
+                  onClick={() => incrementItem(id)}
+                >
                   +
                 </button>
               </div>
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -4,23 +4,75 @@ interface ShoppingCartProviderProps {
   children: ReactNode;
 }
 
-interface ShoppingCartContext {
-  getItemCount: (id: number) => number;
-  increaseCartCount: (id: number) => void;
-  decreaseCartCount: (id: number) => void;
-  removeFromCart: (id: number) => void;
+interface CartContextType {
+  getItemQuantity: (id: number) => number;
+  incrementItem: (id: number) => void;
+  decrementItem: (id: number) => void;
 }
 
-const ShoppingCartContext = createContext({});
+export interface CartItem {
+  id: number;
+  quantity: number;
+}
+
+const ShoppingCartContext = createContext({} as CartContextType);
 
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
 }
 
-export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    const retrievedItem = cartItems.find((item) => item.id === id);
+    return retrievedItem?.quantity || 0;
+  }
+
+  function incrementItem(id: number) {
+    setCartItems((prev) => {
+      // check if item is already in cart
+      const retrievedItem = prev.find((item) => item.id === id);
+      // if the item is already in the cart, just increment the quantity
+      if (retrievedItem) {
+        return prev.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: retrievedItem.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      } else {
+        return [...prev, { id, quantity: 1 }];
+      }
+    });
+  }
+
+  function decrementItem(id: number) {
+    setCartItems((prev) => {
+      // check if item is already in cart
+      const retrievedItem = prev.find((item) => item.id === id);
+      if (retrievedItem) {
+        return prev.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: retrievedItem.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      } else {
+        return [...prev];
+      }
+    });
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider
+      value={{ getItemQuantity, incrementItem, decrementItem }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
-}
+};
