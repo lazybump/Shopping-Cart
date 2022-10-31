be8e43aa559ae41461c3508e5c002b962956f71a
lazybump <104283344+lazybump@users.noreply.github.com>
2022-10-31 20:30:10 +0000

    remove console log

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -2,7 +2,6 @@ import { useShoppingCart } from "../context/ShoppingCartContext";
 
 function Overlay() {
   const { isOpen } = useShoppingCart();
-  console.log(isOpen);
   return (
     <div
       className={`bg-black absolute inset-0 z-10 pointer-events-none transition-opacity duration-200 ${
